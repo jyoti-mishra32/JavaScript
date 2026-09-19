882f60ed1a486d3537040f96e0da6876c0a9aa60
jyoti-mishra32 <kumarijyotimishra264@gmail.com>
2026-09-19 21:12:54 +0530

    Create script.js

---
+++ Js Variables/script.js
@@ -0,0 +1,14 @@
+//let 
+let age = 22;
+age = 23;
+
+//const
+const country = "India";
+
+//var
+var name = "Jyoti";
+
+document.getElementById("demo").innerHTML = 
+"Age: " + age + "<br>" +
+"Country: " + country + "<br>" +
+"name: " + name;
